7b5400f5732fd1e1b045465478c1e354644d1756
Kishan Patel <kpatel@savanacorp.net>
2020-10-16 13:52:54 -0400

    v1

--- vars.tf
+++ vars.tf
@@ -15,6 +15,9 @@ variable "subnetCIDRblock_private" {
 variable "subnetCIDRblock_public" {
   default =  "192.168.1.0/24"
 }
+variable "subnetCIDRblock_my" {
+  default =  "192.168.3.0/24"
+}
 variable "mapPublicIP" {
   default = true
 }
