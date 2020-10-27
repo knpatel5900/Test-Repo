c8386d6688d916c1c475da53f36d725930aff574
Kishan Patel <kpatel@savanacorp.net>
2020-10-27 08:28:14 -0400

    Chanhes made on  Tue, Oct 27, 2020  8:27:54 AM

--- git.sh
+++ git.sh
@@ -1,5 +1,6 @@
  #!/bin/bash
 git add .
 dte=`date`
-git commit -m "Chanhes made on  $dte"
+echo -n "Please enter the message for commit changes"; read message;
+git commit -m " $massage Chanhes made on  $dte"
 git push -u origin master
