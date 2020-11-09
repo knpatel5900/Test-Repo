1019aba324a383812ff7e207ed0c34e76bc14cac
Kishan Patel <kpatel@savanacorp.net>
2020-11-09 13:23:09 -0500

    SD:SD-323 -- for creditone -- changes made on -- Mon, Nov  9, 2020  1:22:52 PM

--- git-Test.sh
+++ git-Test.sh
@@ -1,7 +1,7 @@
  #!/bin/bash
 git add .
 dte=`date`
-echo -n Please enter your name:-- ; read name;
+echo -n Please enter your SD-Ticket-No:-- ; read Tno;
 echo -n Please enter Message for commit changes:-- ; read message;
-git commit -m "---- $name ---- $message ---- changes made on -- $dte"
+git commit -m "SD:$Tno -- $message -- changes made on -- $dte"
 git push -u origin master
