ea19148b1a26a55956b4a6849498f70313e8ed82
Kishan Patel <kpatel@savanacorp.net>
2020-11-09 11:29:15 -0500

    ---- Kishan---- V2 Chanhes made on

--- git-Test.sh
+++ git-Test.sh
@@ -1,6 +1,6 @@
  #!/bin/bash
 git add .
-dte=`date + "%d/%h/%Y-%T"`
+dte=`date + '%d/%h/%Y-%T'`
 echo -n Please enter your name:-- ; read name;
 echo -n Please enter Message for commit changes:-- ; read message;
 git commit -m "---- $name---- $message Chanhes made on  $dte"
