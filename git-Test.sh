a3b46fcfb1e2756944a595e02abd32ae7b53a353
Kishan Patel <kpatel@savanacorp.net>
2020-11-09 11:31:39 -0500

    ---- kishan---- v4 ---- Changes made on  Mon, Nov  9, 2020 11:31:35 AM

--- git-Test.sh
+++ git-Test.sh
@@ -3,5 +3,5 @@ git add .
 dte=`date`
 echo -n Please enter your name:-- ; read name;
 echo -n Please enter Message for commit changes:-- ; read message;
-git commit -m "---- $name---- $message Chanhes made on  $dte"
+git commit -m "---- $name---- $message ---- Changes made on  $dte"
 git push -u origin master
